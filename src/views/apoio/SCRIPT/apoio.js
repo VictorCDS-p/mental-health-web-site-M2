6636fd44c6258534a517f73de69659410c358f0c
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-04-09 07:26:30 -0300

    feat: adiciona logo e link do site no cadastro das instituições

--- src/views/apoio/SCRIPT/apoio.js
+++ src/views/apoio/SCRIPT/apoio.js
@@ -1,45 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
     const institutions = [
         {
+            logo: "./img/canto-baoba.png",
             name: "Canto Baobá",
             region: "Todo Brasil",
             type: "Online e Presencial",
-            description: "Espaço especializado em saúde mental e diversidade, com ênfase no enfrentamento às violências estruturais. Oferece psicoterapia (online e presencial) e orientação vocacional/profissional."
+            description: "Espaço especializado em saúde mental e diversidade, com ênfase no enfrentamento às violências estruturais.",
+            link: "https://www.cantobaoba.com.br/"
         },
         {
+            logo: "./img/viver-mais-psi.jpg",
             name: "Viver Mais Psicologia",
             region: "Todo Brasil",
             type: "Online e Presencial",
-            description: "Oferece serviços de saúde emocional e psicológica com valor social. Atendimentos realizados por psicólogos psicoterapeutas em formação e pós-graduandos."
+            description: "Oferece serviços de saúde emocional e psicológica com valor social. Atendimentos realizados por psicólogos psicoterapeutas em formação e pós-graduandos.",
+            link: "https://vivermaispsicologia.com.br/atendimento-psicologico-viver/"
         },
         {
+            logo: "./img/casa1.png",
             name: "Casa 1",
             region: "São Paulo",
             type: "Presencial",
-            description: "Oferece acolhida para jovens LGBT+ e é uma clínica de saúde mental gratuita."
+            description: "Oferece acolhida para jovens LGBT+ e é uma clínica de saúde mental gratuita.",
+            link: "https://www.casaum.org/"
         },
         {
+            logo: "./img/pode-falar.svg",
             name: "Pode Falar",
             region: "Todo Brasil",
             type: "Online",
-            description: " Um canal de ajuda em saúde mental para jovens de 13 a 24 anos, disponível todos os dias, das 08h às 22h, horário de Brasília."
+            description: "Um canal de ajuda em saúde mental para jovens de 13 a 24 anos, disponível todos os dias, das 08h às 22h, horário de Brasília.",
+            link: "https://www.podefalar.org.br/"
         }
     ];
 
-    const institutionsContainer = document.getElementById('institutions');
+    const slider = document.getElementById('slider');
 
     function displayInstitutions(institutions) {
-        institutionsContainer.innerHTML = '';
+        slider.innerHTML = '';
         institutions.forEach(function(institution) {
             const card = document.createElement('div');
             card.classList.add('card');
             card.innerHTML = `
-                <h3>${institution.name}</h3>
-                <p><strong>Região:</strong> ${institution.region}</p>
-                <p><strong>Tipo:</strong> ${institution.type}</p>
-                <p>${institution.description}</p>
+                <div class="card-content">
+                    <img class="cvv-logo" alt="${institution.name} Logo">
+                    <h2 class="name">${institution.name}</h2>
+                    <p class="region"><strong>Região:</strong> ${institution.region}</p>
+                    <p class="type"><strong>Tipo:</strong> ${institution.type}</p>
+                    <p class="description">${institution.description}</p>
+                    <a href="${institution.link}" class="button view-more">Mais</a>
+                </div>
             `;
-            institutionsContainer.appendChild(card);
+            const imgUrl = card.querySelector('.cvv-logo');
+            imgUrl.setAttribute('src', institution.logo);
+
+            console.log("Caminho da imagem:", institution.logo);
+            slider.appendChild(card);
         });
     }
 
