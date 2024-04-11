d22ab9cdd3efa0f91f3e76ca25af229d18057011
VictorCDS-p <Victor90774@outlook.com>
2024-04-10 21:04:20 -0300

    responsividade(parte1) + menu navegacional mobile

--- src/views/home/script.js
+++ src/views/home/script.js
@@ -1,1 +1,14 @@
-console.log("Oi")
+let $btnMenu = document.getElementById("btnMenu");
+let $menu = document.getElementById("menuMobile");
+let $overlay = document.getElementById("overlayMenu")
+$btnMenu.addEventListener("click", ()=>{
+    $menu.classList.add("abrirMenu")
+})
+
+$menu.addEventListener("click", ()=>{
+    $menu.classList.remove("abrirMenu")
+})
+
+$overlay.addEventListener("click", ()=>{
+    $menu.classList.remove("abrirMenu")
+})
